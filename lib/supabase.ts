ff3cf0f6de6bb5ac4b5dfa260cd6618007c8e727
tania061108 <fanesiat@gmail.com>
2026-09-24 13:04:08 +0700

    fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,12 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
 
+// Tidak menggunakan throw new Error agar proses next build di Vercel tidak gagal
 if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error('Supabase URL atau Service Role Key belum terpasang di environment variables!');
+  console.warn('Peringatan: Supabase URL atau Service Role Key belum terpasang!');
 }
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
